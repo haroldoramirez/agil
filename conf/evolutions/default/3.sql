f8fd59232dfc15aa0c14b0320e27b5adc716bb26
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2020-03-02 14:03:56 -0300

    adicionado inserts do arquivo 3.sql para o arquivo 2.sql

--- conf/evolutions/default/3.sql
+++
@@ -1,8 +0,0 @@
-# --- Sample dataset
-
-# --- !Ups
-
-insert into gasto_especifico (nome, codigo, descricao, natureza_gasto_id, conta_contabil, conta_orcamentaria, data_cadastro, data_alteracao)
-    values ('ME015','11112','Mesas e cadeiras para Central de Bioenergia',28,'1203180003','204 MOVEIS E UTENSILIOS', 'now', 'now');
-insert into gasto_especifico (nome, codigo, descricao, natureza_gasto_id, conta_contabil, conta_orcamentaria, data_cadastro, data_alteracao)
-    values ('ME016','11112','Móveis Equipamentos - Outros',28,'1203180003','204 MOVEIS E UTENSILIOS', 'now', 'now');
