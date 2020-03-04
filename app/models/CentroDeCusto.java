9e22b85374dab4729fb9ea47d3c21e8def2f0e77
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2020-03-04 09:09:38 -0300

    deploy heroku

--- app/models/CentroDeCusto.java
+++ app/models/CentroDeCusto.java
@@ -19,6 +19,8 @@ public class CentroDeCusto extends Model {
     @Column(nullable = false, length = 10)
     private String nome;
 
+    private String nome2;
+
     @Column(nullable = false, length = 45)
     private String nomeCC;
 
